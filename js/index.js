1471459dbe93eff1e4cc2b35b72aa38f62585758
halvorvl <98693773+halvorvl@users.noreply.github.com>
2023-09-06 15:32:57 +0200

    Fixing sliders

--- js/index.js
+++ js/index.js
@@ -1,21 +1,25 @@
 createHtmlHere = document.querySelector(".blog_slider");
+const createHtmlForStickyPosts = document.querySelector(".sticky_posts");
 const prevBtn = document.querySelector(".prev_post");
 const nextBtn = document.querySelector(".next_post");
+const url = "https://www.reisesakte.no//wp-json/wp/v2/posts?_embed";
 
 let currentIndex = 0;
+let startIndex = 0;
 let posts = [];
 
-function getBlogPosts() {
-  fetch("https://www.reisesakte.no//wp-json/wp/v2/posts?_embed")
-    .then((response) => response.json())
-    .then((data) => {
-      posts = data;
+async function getBlogPosts() {
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+    posts = result;
 
-      displayPost(currentIndex);
-    })
-    .catch((error) => {
-      console.error("Error fetching posts:", error);
-    });
+    displayPost(currentIndex);
+
+    displayPosts(startIndex);
+
+    startAutoSlide();
+  } catch (error) {}
 }
 
 function displayPost(index) {
@@ -24,64 +28,79 @@ function displayPost(index) {
     const featuredMedia = post._embedded["wp:featuredmedia"][0];
     const imageUrl = featuredMedia.source_url;
 
-    const blogPost = document.createElement("div");
-    blogPost.classList.add("blog_post_slider");
-    blogPost.innerHTML = `
-                <div>
-     
-                <img src="${imageUrl}" alt="${post.title.rendered}" class="blog_post_image">
-                
+    if (post.sticky === true) {
+      const blogPost = document.createElement("div");
+      blogPost.classList.add("blog_post_slider");
+      blogPost.innerHTML = `
+                <img src="${imageUrl}" alt="${post.title.rendered}">
                 <h2 class="h2_for_slider">${post.title.rendered}</h2>
-
                 
-               
-              
-                </div>
-                 
-                
-                
-               
             `;
 
-    createHtmlHere.innerHTML = "";
-    createHtmlHere.appendChild(blogPost);
+      createHtmlHere.innerHTML = "";
+      createHtmlHere.appendChild(blogPost);
+    }
   }
 }
 
-prevBtn.addEventListener("click", (e) => {
-  currentIndex = (currentIndex - 1 + posts.length) % posts.length;
-  displayPost(currentIndex);
+function startAutoSlide() {
+  autoSlideInterval = setInterval(() => {
+    currentIndex = (currentIndex + 1) % posts.length;
+    displayPost(currentIndex);
+  }, 100005000);
+}
+
+function displayPosts(startIndex) {
+  const endIndex = startIndex + 3;
+  createHtmlForStickyPosts.innerHTML = "";
+
+  for (let i = startIndex; i < endIndex && i < posts.length; i++) {
+    const post = posts[i];
+    const featuredMedia = post._embedded["wp:featuredmedia"][0];
+    const imageUrl = featuredMedia.media_details.sizes.medium.source_url;
+
+    const blogPost = document.createElement("div");
+    blogPost.classList.add("featured_blog_posts");
+    blogPost.innerHTML = `
+                <img src="${imageUrl}" alt="${post.title.rendered}">
+                <h2>${post.title.rendered}</h2>
+                
+            `;
+
+    createHtmlForStickyPosts.appendChild(blogPost);
+  }
+}
+
+prevBtn.addEventListener("click", function () {
+  startIndex = Math.max(startIndex - 3, 0);
+  displayPosts(startIndex);
 });
 
-nextBtn.addEventListener("click", (e) => {
-  currentIndex = (currentIndex + 1) % posts.length;
-  displayPost(currentIndex);
+nextBtn.addEventListener("click", function () {
+  startIndex = Math.min(startIndex + 3, posts.length - 1);
+  displayPosts(startIndex);
 });
 
 getBlogPosts();
 
-const createHtmlForStickyPosts = document.querySelector(".sticky_posts");
+//async function getFeaturedPosts() {
+// {
+// const response = await fetch(url);
 
-const url = "https://www.reisesakte.no//wp-json/wp/v2/posts?_embed";
+//const result = await response.json();
 
-async function getFeaturedPosts() {
-  {
-    const response = await fetch(url);
+//for (let i = 0; i < result.length; i++)
+// if (result[i].sticky === true) {
+// const featuredMedia = result[i]._embedded["wp:featuredmedia"][0];
+//const imageUrl = featuredMedia.media_details.sizes.medium.source_url;
 
-    const result = await response.json();
+// createHtmlForStickyPosts.innerHTML += `<div class="featured_blog_posts"><img class="featured_blog_images" src="${imageUrl}" alt="${result[i].title.rendered}">
+//       <h2 class="featured_h2">${result[i].title.rendered}</h2>
+//     ${result[i].excerpt.rendered} </div> `;
+// }
 
-    for (let i = 0; i < result.length; i++)
-      if (result[i].sticky === true) {
-        const featuredMedia = result[i]._embedded["wp:featuredmedia"][0];
-        const imageUrl = featuredMedia.media_details.sizes.medium.source_url;
+// console.log(result);
+// }
+//}
 
-        createHtmlForStickyPosts.innerHTML += `<div class="featured_blog_posts"><img class="featured_blog_images" src="${imageUrl}" alt="${result[i].title.rendered}">
-                <h2 class="featured_h2">${result[i].title.rendered}</h2>
-                ${result[i].excerpt.rendered} </div> `;
-      }
-
-    console.log(result);
-  }
-}
-
-getFeaturedPosts();
+//getFeaturedPosts();
