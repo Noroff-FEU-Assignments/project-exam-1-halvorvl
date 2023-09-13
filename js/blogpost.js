e479966100b399dca996c9727f759ecc841f2b68
halvorvl <98693773+halvorvl@users.noreply.github.com>
2023-09-13 15:29:42 +0200

    remove comments

--- js/blogpost.js
+++ js/blogpost.js
@@ -47,11 +47,10 @@ async function getProductsDetails() {
   single_post.appendChild(content);
 
   blogImage.addEventListener("click", function () {
-    event.stopPropagation(); // Prevent the click event from propagating to the body
+    event.stopPropagation();
     overlayImage.style.display = "flex";
     console.log("Overlay clicked!");
 
-    // Attach a click event listener to the body
     document.body.addEventListener("click", bodyClick);
   });
 
@@ -59,7 +58,6 @@ async function getProductsDetails() {
     overlayImage.style.display = "none";
     console.log("Body clicked!");
 
-    // Remove the click event listener from the body
     document.body.removeEventListener("click", bodyClick);
   };
 
@@ -67,26 +65,3 @@ async function getProductsDetails() {
 }
 
 getProductsDetails();
-
-// createHtmBlogSpecific.addEventListener("click", function () {
-// bigImage.style.display = "flex";
-// getProductsDetails();
-//});
-
-//bigImage.addEventListener("click", function () {
-// bigImage.style.display = "none";
-// getProductsDetails();
-// });
-
-/// createHtmBlogSpecific.innerHTML = `<div class="single_post">
-
-//  <h2> ${result.title.rendered} </h2>
-
-//  <img class="overlay"
-//  src="${result._embedded["wp:featuredmedia"]["0"].source_url}"/>
-
-//  <img class="blog_image"
-// src="${result._embedded["wp:featuredmedia"]["0"].source_url}"/>
-// <p class="content"> ${result.content.rendered} </p>
-
-//`;
